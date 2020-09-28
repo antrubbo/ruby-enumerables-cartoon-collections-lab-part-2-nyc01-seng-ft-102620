4981a3db73818eea7e9a1a75fc0f6844b13ac8ad
Anthony Rubbo <anthony.rubbo@gmail.com>
2020-09-28 20:35:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,17 +14,12 @@ def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   index = 0
   good_call = nil
-  planeteer_calls.find do |call|
-    call == valid_calls[index]
-    good_call = call
-    index += 1 
+  planeteer_calls.each do |call|
+    if call == valid_calls[index]
+      good_call = call
+    else
+      index += 1
+    end
   end
-  # planeteer_calls.each do |call|
-  #   if call == valid_calls[index]
-  #     good_call = call
-  #   else
-  #     index += 1
-  #   end
-  # end
   good_call
 end
